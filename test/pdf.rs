6f97c4e9a8dcc4aabd82931309283b57f86fbb56
Zack Corr <zackcorr95@gmail.com>
2012-01-13 14:21:27 +1000

    Updated special surface tests, now flushes the surfaces before ending the program.

--- test/pdf.rs
+++ test/pdf.rs
@@ -11,6 +11,8 @@ fn test() {
 	context.set_source_rgb(1.0, 0.0, 0.0);
 	context.rectangle(10.0, 10.0, 80.0, 80.0);
 	context.fill();
+	surface.flush();
 	
+	assert surface.get_status() == cairo::STATUS_SUCCESS;
 	assert context.get_status() == cairo::STATUS_SUCCESS;
 }
