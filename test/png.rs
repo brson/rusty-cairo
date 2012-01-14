e0def82511dae2db3b73571226ff27d8cf1c3fc1
Zack Corr <zackcorr95@gmail.com>
2012-01-14 11:10:24 +1000

    Made the png test not throw errors when it shouldn't be (merged two tests into one).

--- test/png.rs
+++ test/png.rs
@@ -2,9 +2,20 @@ use std;
 use cairo;
 
 #[test]
-fn png_b() {
-	let surface: cairo::surface = cairo::mk_image_surface_from_file("png-1.png");
+fn png() {
+	let surface: cairo::surface = cairo::mk_image_surface(cairo::FORMAT_RGB24, 200u, 100u);
 	let context: cairo::context = cairo::mk_context(surface);
+	
+	context.set_source_rgb(0.0, 0.0, 0.0);
+	context.paint();
+	
+	assert context.get_status() == cairo::STATUS_SUCCESS;
+	assert surface.write_to_file("png-1.png") == cairo::STATUS_SUCCESS;
+	
+	surface.flush();
+	
+	surface = cairo::mk_image_surface_from_file("png-1.png");
+	context = cairo::mk_context(surface);
 	
 	context.set_source_rgb(0.0, 1.0, 1.0);
 	context.rectangle(40.0, 10.0, 120.0, 80.0);
@@ -14,16 +25,6 @@ fn png_b() {
 	assert surface.get_image_width() == 200u;
 	assert surface.get_image_height() == 100u;
 	assert surface.write_to_file("png-2.png") == cairo::STATUS_SUCCESS;
-}
-
-#[test]
-fn png_a() {
-	let surface: cairo::surface = cairo::mk_image_surface(cairo::FORMAT_RGB24, 200u, 100u);
-	let context: cairo::context = cairo::mk_context(surface);
 	
-	context.set_source_rgb(0.0, 0.0, 0.0);
-	context.paint();
-	
-	assert context.get_status() == cairo::STATUS_SUCCESS;
-	assert surface.write_to_file("png-1.png") == cairo::STATUS_SUCCESS;
+	surface.flush();
 }
